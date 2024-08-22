ddb0a940ee91bd3e38dd0ca935e57400f2e40f81
incubox <be.salasm@duocuc.cl>
2024-08-22 15:21:25 -0400

    mostrar

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  tokensUrl: 'https://liimeyoinrftwakomrqs.supabase.co/rest/v1/tokens',
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpaW1leW9pbnJmdHdha29tcnFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NzIzNzAsImV4cCI6MjAzNzM0ODM3MH0.KGRZUVCHgmBJZcuMIa09wX7ABmk6FkvSzWmz2xY4in4',
-  apiUrl: 'https://liimeyoinrftwakomrqs.supabase.co/rest/v1/users',
-  pedUrl: 'https://liimeyoinrftwakomrqs.supabase.co/rest/v1/pedidos',
-  URL: 'https://liimeyoinrftwakomrqs.supabase.co'
-};
-
