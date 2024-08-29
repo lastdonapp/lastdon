54500dc6da82c1b30415e1156dd51e413624727b
outlander258 <jo.castillog@duocuc.cl>
2024-08-29 16:32:53 -0400

    fix filtro pedidos pagados

--- src/app/conductor-menu/agregar-pedidos/agregar-pedidos.page.ts
+++ src/app/conductor-menu/agregar-pedidos/agregar-pedidos.page.ts
@@ -1,8 +1,19 @@
-// agregar-pedidos.page.ts
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { SupabaseService } from 'src/app/services/supabase.service';
+
+interface Pedido {
+  id: string;
+  pagado: boolean;
+  nombre_pedido?: string;
+  fecha?: string;
+  direccion_pedido?: string;
+  direccion_entrega?: string;
+  comuna?: string;
+  telefono?: string;
+  // Otros campos relevantes
+}
 
 @Component({
   selector: 'app-agregar-pedidos',
@@ -10,7 +21,7 @@ import { SupabaseService } from 'src/app/services/supabase.service';
   styleUrls: ['./agregar-pedidos.page.scss'],
 })
 export class AgregarPedidosPage implements OnInit {
-  pedidos: any[] = [];
+  pedidos: Pedido[] = [];
   usuario: any = this.supabaseService.getCurrentUser(); // Deberías obtener esto desde la sesión o estado del usuario
 
   constructor(
@@ -25,29 +36,26 @@ export class AgregarPedidosPage implements OnInit {
 
   async loadPedidosPorTomar() {
     try {
-      this.pedidos = await this.supabaseService.getPedidosPorTomar();
+      const allPedidos: Pedido[] = await this.supabaseService.getPedidosPorTomar();
+      this.pedidos = allPedidos.filter((pedido: Pedido) => pedido.pagado === true);
     } catch (error) {
       console.error('Error al cargar pedidos por tomar:', error);
     }
   }
 
   async verDetalles(id: string) {
-    // Navega a la página de detalles del pedido
     this.router.navigate(['conductor-menu/detalles-pedido', id]);
   }
 
   async tomarPedido(pedidoId: string) {
     try {
       await this.supabaseService.tomarPedido(pedidoId, this.usuario.email);
-      // Mostrar mensaje de éxito
       const toast = await this.toastController.create({
         message: 'Pedido tomado con éxito',
         duration: 2000,
         color: 'success'
       });
       await toast.present();
-      
-      // Recargar los pedidos después de tomar uno
       this.loadPedidosPorTomar();
     } catch (error) {
       console.error('Error al tomar el pedido:', error);
