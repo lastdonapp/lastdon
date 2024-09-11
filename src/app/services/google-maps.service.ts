a8ab6059977b79ee2eebc84a1bc9e0bebe1a6a27
outlander258 <jo.castillog@duocuc.cl>
2024-09-11 16:46:57 -0300

    geocoding, marker currentlocation

--- src/app/services/google-maps.service.ts
+++ src/app/services/google-maps.service.ts
@@ -1,22 +1,19 @@
+/// <reference types="google.maps" />
 import { Injectable } from '@angular/core';
-declare var google: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleMapsService {
 
-  map: any;
-  markers: any[] = [];
+  map: google.maps.Map | undefined;
+  markers: google.maps.Marker[] = [];
 
   constructor() { }
 
-
-
-
-   // Método para inicializar el mapa
-   initMap(mapElement: HTMLElement, lat: number, lng: number): void {
-    const mapOptions = {
+  // Método para inicializar el mapa
+  initMap(mapElement: HTMLElement, lat: number, lng: number): void {
+    const mapOptions: google.maps.MapOptions = {
       center: new google.maps.LatLng(lat, lng),
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -43,10 +40,22 @@ export class GoogleMapsService {
     }
     this.markers = [];
   }
+ // geocoder API
+  geocodeAddress(address: string): Promise<{latitude: number, longitude: number}> {
+    return new Promise((resolve, reject) => {
+      const geocoder = new google.maps.Geocoder();
+      
+      geocoder.geocode({ address: address }, (results: google.maps.GeocoderResult[] | null, status: google.maps.GeocoderStatus) => {
+        if (status === google.maps.GeocoderStatus.OK && results && results[0]) {
+          const lat = results[0].geometry.location.lat();
+          const lng = results[0].geometry.location.lng();
+          resolve({ latitude: lat, longitude: lng });
+        } else {
+          reject('No se pudo obtener la ubicación: ' + status);
+        }
+      });
+    });
+  }
+
+
 }
-
-
-
-
-
-
