0914ff16e8a56ff2eb5d421ff8becae9dd2f481f
outlander258 <jo.castillog@duocuc.cl>
2024-10-15 19:54:43 -0300

    QR básico

--- src/app/menu/pedidos/pedidos.module.ts
+++ src/app/menu/pedidos/pedidos.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { QrCodeModule } from 'ng-qrcode';
 
 import { PedidosPageRoutingModule } from './pedidos-routing.module';
 
@@ -13,7 +13,8 @@ import { PedidosPage } from './pedidos.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PedidosPageRoutingModule
+    PedidosPageRoutingModule,
+    QrCodeModule
   ],
   declarations: [PedidosPage]
 })
